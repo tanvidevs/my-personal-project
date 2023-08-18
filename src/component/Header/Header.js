a31382254ce80ba2ef4f13d643cdf2205ffa6be1
Tanvi Mirase <112555978+tanvidevs@users.noreply.github.com>
2023-08-18 15:05:33 +0530

    Header.js

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className=' sticky top-0 bg-gray-100'>
+    <div className=' sticky top-0 bg-white'>
       <header className="text-gray-600 body-font shadow-md" >
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
